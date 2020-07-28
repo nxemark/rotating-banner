5a4e282f9a84570ea2b64a7f103e50ef447b3a6b
Mark Okedoyin <markokedoyin@gmail.com>
2020-07-28 12:12:05 -0400

    Sample-for-projects

---
+++ Javascript/script.js
@@ -0,0 +1,36 @@
+// var intro = "My name is Ezekiel"
+// if(intro.indexOf("Mark") !== -1){
+//    alert("Hello Mark");
+// }
+// else{
+//     alert("Hello " + intro.slice(11));
+// }
+
+// var name = intro.replace(intro.slice(11), "Mark")
+// alert(name);
+
+// var name = "Mark, Mary";
+// var mark = name.split(",");
+// console.log(mark);
+// var person = "";
+// for (var n = 0; n < mark.length; n++) {
+//     person += mark[n];
+// }
+// console.log(person);
+
+// function showhistory(){
+//     return document.getElementById("history").style.display = "block";
+// 
+
+var names, out, i, text;
+names = ["Mark", "Mary", "Ezekiel", "Victor"];
+text = names.length;
+console.log(names[2]);
+
+out = "<ol>";
+for (i = 0; i< text; i++) {
+    out += "<li>" + names[i] + "</li>";
+}
+out += "</ol>";
+document.getElementById("mack").innerHTML = out;
+
